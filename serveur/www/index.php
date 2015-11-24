b1418e6b7ddfea086d103c706f81dbcf97a728fd
Hugo Lefèvre <hugo.lefevre@gmail.com>
2015-11-24 18:46:09 +0100

    Amélioration du contrôleur principal

--- serveur/www/index.php
+++ serveur/www/index.php
@@ -3,14 +3,11 @@
  * Cntrôleur global du site
  *
  * Traite les paramètres $_GET.
- * Appele le contrôleur correspondant en fonction de la page demandée. 
  * Créer la connexion à la base de données.
  *
  *
  */
-include_once('modele/connexion_sql.php');
-if (!isset($_GET['section']) OR $_GET['section'] == 'index')
-{
+	include_once('modele/connexion_sql.php');  
     include_once('controleur/index.php');
-}
+
  ?>
