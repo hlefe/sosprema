72469e8a193aecc4db871b4880e81d27dded65e0
komuros <komuros63@hotmail.fr>
2015-11-25 22:28:05 +0100

    correction des commentaire
    correction des commentaire des fonction

--- serveur/www/controleur/nettoyage.php
+++ serveur/www/controleur/nettoyage.php
@@ -2,26 +2,26 @@
 
 class validation {
 	
-	//valide une adresse email donné en paramétre, et retourne true si elle est valide ou false sinon.
+	//nettoie une adresse email donné en paramétre, et retourne true si elle est valide ou false sinon.
 	public static function nettoyerEmail($email){
-		return filter_var($email, FILTER_SANITIZE_EMAIL)==false?false:true;
+		return filter_var($email, FILTER_SAnettoie_EMAIL)==false?false:true;
 
 	//nettoie une chaine de caractére donné en paramétre, et retourne true si elle est valide ou false sinon.
 	public static function nettoyerChaine($chaine){
 		return filter_var($chaine, FILTER_SANITIZE_STRING)==false?false:true;
 	}
 
-	//valide un nombre entier donné en paramétre, et retourne true si il est valide ou false sinon.
+	//nettoie un nombre entier donné en paramétre, et retourne true si il est valide ou false sinon.
 	public static function nettoyerNombreEntier($nombre){
 		return filter_var($nombre, FILTER_SANITIZE_NUMBER_INT)==false?false:true;
 	}
 
-	//valide une adresse url donné en paramétre, et retourne true si elle est valide ou false sinon.
+	//nettoie une adresse url donné en paramétre, et retourne true si elle est valide ou false sinon.
 	public static function nettoyerURL($url){
 		return filter_var($url, FILTER_SANITIZE_URL)==false?false:true;
 	}
 
-	//valide un nombre réel donné en paramétre, et retourne true si il est valide ou false sinon.
+	//nettoie un nombre réel donné en paramétre, et retourne true si il est valide ou false sinon.
 	public static function nettoyerNombreReel($url){
 		return filter_var($url, FILTER_SANITIZE_NUMBER_FLOAT)==false?false:true;
 	}
