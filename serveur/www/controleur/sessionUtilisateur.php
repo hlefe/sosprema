743ea8e8054d6a87346cbba6edd41661eb13e373
komuros <komuros63@hotmail.fr>
2015-11-27 18:57:06 +0100

    mise a niveau et création
    mise à niveau du fichier nettoyage.php
    et création d'un fichier sesssionUtilisateur.php qui gére la session de
    l'utilisateur

---
+++ serveur/www/controleur/sessionUtilisateur.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once(nettoyage.php);
+require_once(validation.php);
+
+class sessionUtilisateur{
+	public static creationSessionUtilisateur(){
+
+		if(isset($_POST['emailConnexion'])){
+
+			$emailConnexion = nettoyage::nettoyerEmail($_POST['emailConnexion']);
+
+			if(validation::validerEmail($emailConnexion)){
+
+				if(isset($_POST['passwordConnexion'])){
+
+					$passwordConnexion = nettoyage::nettoyerChaine($_POST['passwordConnexion']);
+
+					if(validation::validerPassword($passwordConnexion)){
+
+						session_start();
+						try{
+							$utilisateur = new utilisateur($emailConnexion, $passwordConnexion);//a corriger selon le model de valentin
+							$_SESSION(['utilisateurConnecter']) = $utilisateur;
+						}catch(Exception e){
+							//a corriger
+						}			
+					}
+				}
+			}
+		}
+		//ajouter un code d'erreur
+	}
+
+
+}
