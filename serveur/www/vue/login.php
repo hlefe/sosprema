7632df75e17d3b4efd8999d13b848de4b3ae94f7
hlefe <hugo.lefevre@gmail.com>
2015-11-28 11:40:22 +0100

    ajout de l'interface de connexion

--- serveur/www/vue/login.php
+++ serveur/www/vue/login.php
@@ -5,4 +5,14 @@
  *
  */
   include('header.php');
+  ?>
+<h1> Affichage moche qui va bientôt changer </h1>
+  <form action="/controleur/sessionUtilisateur.php" method="post">
+    Adresse mail: <input type="text" name="emailConnexion" value="" />
+     
+    Mot de passe: <input type="password" name="passwordConnexion" value="" />
+     
+    <input type="submit" name="connexion" value="Connexion" />
+</form>
+  <?php
     include('footer.php'); ?>
