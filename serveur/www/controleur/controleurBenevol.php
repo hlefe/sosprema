c827303353a0de0341299064d20e99fd4c85ad87
Raythen95 <nicolas.audinat@gmail.com>
2015-12-07 12:54:07 +0100

    ajout de la redirection vers la page d'accueil

--- serveur/www/controleur/controleurBenevol.php
+++ serveur/www/controleur/controleurBenevol.php
@@ -11,7 +11,7 @@ class controleurBenevol {
         $vueErreur = array();
         session_start();
         echo 'controleur';
-        //if (!isset($_SESSION['utilisateurConnecté'])) {
+        //if (!isset($_SESSION['utilisateurConnecte'])) {
            include_once('vue/login.php');
         //} else {
         //    include_once('vue/accueil.php');
@@ -28,13 +28,12 @@ class controleurBenevol {
             case "validationFormulaire":
                 echo 'validation du formulaire';
                 if(self::validationFormulaireConnexion())
-                    require 'vue/accueil.php';
-                    echo 'Ca marche';
+                    header('Location: vue/accueil.php');                    
                 break;
 
             default :
-                $vueErreur[] = "Problème authentification";
-                require ("/vue/erreur.php");
+                $vueErreur[] = "Probleme authentification";
+                header("vue/erreur.php");
         }
         } catch (Exception $ex) {
             $vueErreur[] = "Erreur inattendue";
@@ -54,9 +53,7 @@ class controleurBenevol {
                     if (validation::validerPassword($passwordConnexion)) {
                         $utilisateur = modelUtilisateur::creationUtilisateurConnecter($emailConnexion, $passwordConnexion);
                         if ($utilisateur != FALSE) {
-                            echo 'not false';
                             $_SESSION['utilisateurConnecter'] = $utilisateur;
-                            echo 'true';
                             return TRUE;
                         } else {
                             return FALSE;
