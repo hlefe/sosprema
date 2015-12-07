43442288c48b483f764fd395529d2ea0c8e2943b
komuros <komuros63@hotmail.fr>
2015-12-07 10:20:04 +0100

    ajout d'un utilisateur

---
+++ serveur/www/vue/ajoutUtilisateur.php
@@ -0,0 +1,17 @@
+<?php include('header.php'); ?>
+
+<h1> Ajouter un utilisateur </h1>
+  <form action="ajoutUtilisateur" method="post">
+    Nom: <input type="text" name="nom" placeholder="nom" />
+    Prenom: <input type="text" name="prenom" placeholder="prenom" />
+    Adresse mail: <input type="text" name="email" placeholder="exemple@gmail.com" />
+    Adresse : </br>
+    Numéro de rue: <input type="text" name="num_rue" placeholder="52" />
+    Nom de la rue: <input type="text" name="nom_rue" placeholder="nom de rue" />
+    Code postal: <input type="text" name="code_postal" placeholder="63000" />
+    Nom de la ville: <input type="text" name="ville" placeholder="ville" />
+     
+    <input type="submit" name="connexion" value="Connexion" />
+</form>
+  
+<?php include('footer.php'); ?>
