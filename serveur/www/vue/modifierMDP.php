4b92190a538178f4c22cfee8cdcac1d19ee543b2
komuros <komuros63@hotmail.fr>
2015-12-13 17:13:52 +0100

    mise en ..... des caractére

--- serveur/www/vue/modifierMDP.php
+++ serveur/www/vue/modifierMDP.php
@@ -3,13 +3,13 @@
 <h1> Profil </h1>
   <form method="post" action="index.php?controleur=controleurBenevol&action=modifierMotDePasse">
     <label for="oldMDP">Mot de passe actuel</label>
-    <input required type="text" name="oldMDP" placeholder="mot de passe" />
+    <input required type="password" name="oldMDP" placeholder="mot de passe" />
     
     <label for="prenom">Nouveau mot de passe</label>
-    <input required type="text" name="newMDP" placeholder="mot de passe" />
+    <input required type="password" name="newMDP" placeholder="mot de passe" />
 
     <label for="prenom">Confirmer nouveau mot de passe</label>
-    <input required type="text" name="newMDPVerif" placeholder="mot de passe" />
+    <input required type="password" name="newMDPVerif" placeholder="mot de passe" />
 
     <button class="submit" name="boutonModifier" value="modifierMDP">Modifier</button>
 
