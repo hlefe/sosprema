bc64d22c919fe0309f4fd5fae2cafdb98ff80a57
hlefe <hugo.lefevre@gmail.com>
2016-01-20 18:49:45 +0100

    effacement général

--- serveur/www/vue/sidebar.php
+++
@@ -1,8 +0,0 @@
-<aside class="sidebar">
-	<section class="box">
-		<header>
-			<h1 id="titre-site"><a href="index.php?action=accueil" rel="home"><img src="vue/images/logo.png"></a></h1>
-		</header>
-        <?php require_once('sidebar-navigation.php'); ?>
-	</section>
-</aside>
