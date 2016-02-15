aaf2f470fd4e63be7a295ac1490b83964e59e65f
hlefe <hugo.lefevre@gmail.com>
2016-02-15 10:09:03 +0100

    Vue contacts locaux

--- serveur/www/vue/pages/moderateur/modificationHopital.php
+++ serveur/www/vue/pages/moderateur/modificationHopital.php
@@ -30,7 +30,15 @@
             
                 <!-- Colonne de droite-->
             <section class="col-lg-5 connectedSortable">
-            
+                <div class="box">
+                    <!-- Champs adresse -->
+                    <?php include('vue/includes/hopital/contacts/contactsLocaux.php'); ?>
+                    <div class="box-footer">
+                            <!-- bouton enregistrer -->
+                            <button type="submit" class="btn btn-primary" name="enregistrer" value="enregistrer">Enregistrer</button>
+                    </div>
+                </div>
+                
                 <div class="box">
                     <!-- Champs adresse -->
                     <?php include('vue/includes/hopital/adresse/adresse.php'); ?>
