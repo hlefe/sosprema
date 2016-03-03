df284c00d43b0072a87d93499672eed8648adaeb
hlefe <hugo.lefevre@gmail.com>
2016-03-03 21:18:48 +0100

    Vue télephones avec bouton ajouter
    boucle à compléter

--- serveur/www/vue/includes/userEdit/informations/informations.php
+++ serveur/www/vue/includes/userEdit/informations/informations.php
@@ -34,4 +34,37 @@ Les champs suivants sont définis:
             <label for="email">Adresse Mail</label>
             <input class="form-control" required type="email" name="email" value="<?php if(isset($utilisateur)) echo $utilisateur->email;?>" />
     </div>
+    <div class="form-group">
+        <label>Téléphones:</label>
+        
+        <!-- Boucle for pour chaque numéro de cet utilisateur, l'afficher 
+       <?php 
+                    //    foreach($listenumero as $numero){
+        
+        ?>
+        -->
+        <div class="input-group">
+            <div class="input-group-addon">
+            <i class="fa fa-phone"></i>
+            </div>
+            <input class="form-control " data-inputmask="&quot;mask&quot;: &quot;(999) 999-9999&quot;" data-mask="" type="text" content="<?php echo $numero; ?>">
+            <div class="input-group-addon ">
+                <a href="">
+                    <i class="fa fa-close btn-danger"></i>
+                </a>
+            </div>    
+        </div>
+        
+         <!-- Fin de la boucle
+       <?php 
+                    // }
+        ?>
+        -->
+        <div class="btn btn-default btn-file pull-right">
+                <i class="fa fa-plus"></i> Ajouter un autre numéro
+                <input name="ajouter" type="file" >
+        </div>
+       
+
+     </div>
 </div>
