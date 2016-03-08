86c94c551daa1dbe9c6dc07c824079cd6381cb58
hlefe <hugo.lefevre@gmail.com>
2016-03-08 17:11:46 +0100

    Réparation erreur bdd

--- serveur/www/vue/includes/hopital/contacts/contactsLocaux.php
+++ serveur/www/vue/includes/hopital/contacts/contactsLocaux.php
@@ -1,21 +1,24 @@
 <div class="box-header with-border" style="cursor: initial; ">
     <h3 class="box-title" style="cursor: initial; ">Contacts locaux pour cet hôpital</h3>
-    <?php if($_REQUEST['action'] != "afficherHopital") { ?>
-    <?php } ?>
 </div>
+
 <ul class="users-list clearfix">
+    <?php if(isset($hopital->listeContactLocal)){
+        if($hopital->listeContactLocal != null){
+            foreach($hopital->listeContactLocal as $contact){
+    ?>
     <li>
         <img src="vue/style/dist/img/user1-128x128.jpg" alt="User Image">
-        <a class="users-list-name" href="#">Fabrice Opry</a>
+        <a class="users-list-name" href="#">Fabrice Opry <?php echo $contact->nom; ?></a>
         <span class="users-list-date">06 86 60 02 12</span>
     </li>
-    <li>
-        <img src="vue/style/dist/img/user8-128x128.jpg" alt="User Image">
-        <a class="users-list-name" href="#">Richard Ruiny</a>
-        <span class="users-list-date">09 87 99 55 43</span>
-    </li>
+    <?php 
+            }}}
+    ?>
+    <?php if($_REQUEST['action'] != "afficherHopital") { ?>
     <li>
         <img src="vue/images/users-add-user-icon.png" alt="User Image">
         <a class="users-list-name" href="#">Ajouter</a>
     </li>
+     <?php } ?>
 </ul>
