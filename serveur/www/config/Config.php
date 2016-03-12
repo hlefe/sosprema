1a07ddb3097cc3574e8f5308f4d8ddb19a87b04b
hlefe <hugo.lefevre@gmail.com>
2016-03-12 15:18:58 +0100

    Plantage github

--- serveur/www/config/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$rep=__DIR__.'/../';
-
-$dsn='mysql:host=localhost;dbname=sosprema';
-$user='root';
-$pswd='sosprema';
-
-//<?php echo $_SERVER['PHP_SELF'];
--- serveur/www/vue/includes/erreur.php
+++
@@ -1,9 +0,0 @@
-<?php if(isset($vueErreur)){ ?>
-		<?php
-		//foreach ($vueErreur as $key => $value) {
-		//	echo $value;
-		//}
-        print_r($vueErreur);
-        
-        ?>
-<?php } ?>
